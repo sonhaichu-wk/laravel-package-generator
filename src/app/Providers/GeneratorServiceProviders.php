730cefc3d32554e0a80c344a1dfa2c4ac666d660
sonhaichu <sonhaichu@gmail.com>
2019-11-01 13:56:37 +0700

    init

---
+++ src/app/Providers/GeneratorServiceProviders.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace HaiCS\Laravel\Generator\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Generator service provider
+ */
+class GeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register any application services
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
