717cf2eeea33498d5a0ce799771d3412ba34240d
sonhaichu <sonhaichu@gmail.com>
2019-11-05 17:45:51 +0700

    create listener testsuit

---
+++ tests/Commands/CreateListenerTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace HaiCS\Laravel\Generator\Test\Commands;
+
+use HaiCS\Laravel\Generator\Test\Commands\CommandTestCase;
+
+class CreateListenerTest extends CommandTestCase
+{
+    /**
+     * Get create listener command to run
+     *
+     * @return string
+     */
+    protected function getCreateListenerCommand($listener_name)
+    {
+        return 'make:package:listener ' . $this->packageName . ' ' . $listener_name;
+    }
+
+    /**
+     * @test
+     */
+    public function can_create_listener()
+    {
+        $listener_name = 'example';
+
+        $this->artisan($this->getCreatePackageCommand())
+            ->assertExitCode(0);
+
+        $this->artisan($this->getCreateListenerCommand($listener_name))
+            ->expectsOutPut('Test generate successful')
+            ->assertExitCode(0);
+    }
+
+    /**
+     * @test
+     */
+    public function can_create_listener_recursively()
+    {
+        $listener_name = 'example/example';
+
+        $this->artisan($this->getCreatePackageCommand())
+            ->assertExitCode(0);
+
+        $this->artisan($this->getCreateListenerCommand($listener_name))
+            ->expectsOutPut('Test generate successful')
+            ->assertExitCode(0);
+    }
+
+    /**
+     * @test
+     */
+    public function cannot_create_listener_if_listener_already_existed()
+    {
+        $listener_name = 'example/example';
+
+        $this->artisan($this->getCreatePackageCommand())
+            ->assertExitCode(0);
+
+        $this->artisan($this->getCreateListenerCommand($listener_name))
+            ->expectsOutPut('Test generate successful')
+            ->assertExitCode(0);
+
+        $this->artisan($this->getCreateListenerCommand($listener_name))
+            ->expectsOutPut('Test already existed')
+            ->assertExitCode(1);
+    }
+
+    /**
+     * @test
+     */
+    public function cannot_create_listener_if_package_does_not_exist()
+    {
+        $listener_name = 'example/example';
+
+        $this->artisan($this->getCreateListenerCommand($listener_name))
+            ->expectsOutPut('Package does not exist')
+            ->assertExitCode(1);
+    }
+}
