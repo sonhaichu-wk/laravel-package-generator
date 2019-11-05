515ca7b691b0662d8512750a44427d4712a31c24
sonhaichu <sonhaichu@gmail.com>
2019-11-05 17:10:48 +0700

    create transformer testsuite

---
+++ tests/Commands/CreateTransformerTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace HaiCS\Laravel\Generator\Test\Commands;
+
+use HaiCS\Laravel\Generator\Test\TestCase;
+
+class CreateTransformerTest extends TestCase
+{
+    /**
+     * Get create transformer command to run
+     *
+     * @return string
+     */
+    protected function getCreateTransformerCommand($transformer_name)
+    {
+        return 'make:package:entity ' . $this->packageName . ' ' . $transformer_name;
+    }
+
+    /**
+     * @test
+     */
+    public function can_create_transformer()
+    {
+        $transformer_name = 'example';
+
+        $this->artisan($this->getCreatePackageCommand())
+            ->assertExitCode(0);
+
+        $this->artisan($this->getCreateTransformerCommand($transformer_name))
+            ->expectsOutPut('Transformer generate successful')
+            ->assertExitCode(0);
+    }
+
+    /**
+     * @test
+     */
+    public function can_create_transformer_recursively()
+    {
+        $transformer_name = 'example/example';
+
+        $this->artisan($this->getCreatePackageCommand())
+            ->assertExitCode(0);
+
+        $this->artisan($this->getCreateTransformerCommand($transformer_name))
+            ->expectsOutPut('Transformer generate successful')
+            ->assertExitCode(0);
+    }
+
+    /**
+     * @test
+     */
+    public function cannot_create_transformer_if_transformer_already_existed()
+    {
+        $transformer_name = 'example/example';
+
+        $this->artisan($this->getCreatePackageCommand())
+            ->assertExitCode(0);
+
+        $this->artisan($this->getCreateTransformerCommand($transformer_name))
+            ->expectsOutPut('Transformer generate successful')
+            ->assertExitCode(0);
+
+        $this->artisan($this->getCreateTransformerCommand($transformer_name))
+            ->expectsOutPut('Transformer already existed')
+            ->assertExitCode(1);
+    }
+
+    /**
+     * @test
+     */
+    public function cannot_create_transformer_if_package_does_not_exist()
+    {
+        $transformer_name = 'example/example';
+
+        $this->artisan($this->getCreateTransformerCommand($transformer_name))
+            ->expectsOutPut('Package does not exist')
+            ->assertExitCode(1);
+    }
+}
